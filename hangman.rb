bf4f5b55844734b24560df5b0c6caf8943cafdd4
mokeaveny <michaelkeaveny0@gmail.com>
2020-04-14 22:54:08 +0100

    Made the code a bit cleaner by creating a function for getting the dictionary vlaues. Also created a method that creates blank spaces in hangman that tells you the number of letters there are in the random word

--- hangman.rb
+++ hangman.rb
@@ -1,12 +1,13 @@
-words = []
-number_of_guesses = 10
-
-File.open("5desk.txt", "r") do |file|
-	file.readlines.each_with_index do |line| # Iterates over the entire file of words
-		line = line.strip # Strips the whitespace away from the words
-		line_size = line.length
-		words.append(line)if (line_size > 4) && (line_size < 13) # Appends the word to the words array if it is between 5 and 12
+def get_dict
+	words = []
+	File.open("5desk.txt", "r") do |file|
+		file.readlines.each_with_index do |line| # Iterates over the entire file of words
+			line = line.strip # Strips the whitespace away from the words
+			line_size = line.length
+			words.append(line)if (line_size > 4) && (line_size < 13) # Appends the word to the words array if it is between 5 and 12
+		end
 	end
+	return words
 end
 
 class Game
@@ -14,18 +15,26 @@ class Game
 		@number_of_guesses = 10
 		@word_array = dict
 		@the_word = ""
-		pick_random_word # Calls the method to get a random word from the dictionary in the constructor
+		pick_random_word
+		puts @the_word
+		@letter_spaces = []
+		set_letter_spaces
+		print @letter_spaces
 	end
 
 	def pick_random_word
 		@the_word = @word_array[rand(@word_array.length)]
 	end
 
-	def get_word
-		return @the_word
-	end
+	def set_letter_spaces
+		word_length = @the_word.length
 
+		for i in 1..word_length
+			@letter_spaces.append("_ ")
+		end
+	end
+	
 end
 
-new_game = Game.new(words)
-puts new_game.get_word
+new_game = Game.new(get_dict)
+puts new_game
