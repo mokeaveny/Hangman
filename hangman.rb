9ac0c66d5e5f88b25a0b410dc31511caaf94058f
mokeaveny <michaelkeaveny0@gmail.com>
2020-04-14 23:38:58 +0100

    Created a turn method. This method allows the user to enter a letter (checks if it is a correct letter by using an alphabet array I created) it then compares the valid letter with the original word and records if the letter is contained within the random word. If it is then it appends the index position of the letter within the word to an array.

--- hangman.rb
+++ hangman.rb
@@ -3,6 +3,7 @@ def get_dict
 	File.open("5desk.txt", "r") do |file|
 		file.readlines.each_with_index do |line| # Iterates over the entire file of words
 			line = line.strip # Strips the whitespace away from the words
+			line.downcase! # Converts the word to lowercase
 			line_size = line.length
 			words.append(line)if (line_size > 4) && (line_size < 13) # Appends the word to the words array if it is between 5 and 12
 		end
@@ -20,6 +21,8 @@ class Game
 		@letter_spaces = []
 		set_letter_spaces
 		print @letter_spaces
+		print "\n"
+		turn
 	end
 
 	def pick_random_word
@@ -34,6 +37,33 @@ class Game
 		end
 	end
 	
+	def turn
+		valid_letter = false
+		alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+		while valid_letter == false
+			puts "Guess a letter: "
+			letter = gets.chomp
+			letter.downcase!
+			if alphabet.include?(letter)
+				valid_letter = true
+			end
+		end
+		puts letter
+		if @the_word.include?(letter)
+			puts "The letter is included within the hangman word!"
+			letter_index = []
+			word_length = @the_word.length
+			for i in 0...word_length
+				if @the_word[i] == letter
+					letter_index.append(i) # Appends the position in the hangman word at which the letter guessed occured
+				end
+			end
+			puts letter_index
+		else
+			puts "The letter #{letter} is not included within the word!"
+		end
+	end
+		
 end
 
 new_game = Game.new(get_dict)
